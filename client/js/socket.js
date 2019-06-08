e0735338b1401a0efa147e92c19d8f81684f33e1
Shwastoo <jakubszwast@gmail.com>
2019-06-08 18:39:35 +0200

    Tury śmigają i widać je na UI

--- client/js/socket.js
+++ client/js/socket.js
@@ -9,60 +9,84 @@ var EventList = ["check-word",
 
 var SocketHander = {
     client: null,
-    init: function (socket) {
+    init: function(socket) {
         this.client = socket
         this.responseHandlers()
 
-        this.addResponseCallback("session-joined", function (data) {
+        this.addResponseCallback("session-joined", function(data) {
             if (data.myTurn) {
                 Game.myTurn = true
+
                 console.log("teraz moja tura")
             } else {
                 Game.myTurn = false
+
                 console.log("już nie moja tura")
             }
 
         })
-        this.addResponseCallback("session-closed", function (data) {
+        this.addResponseCallback("session-closed", function(data) {
             alert(`Session closed, reason: ${data.reason}`)
         })
 
-        this.addResponseCallback("start-game", function (data) {
-            $("#letterGet").click()
+        this.addResponseCallback("start-game", function(data) {
+            var count = 15
+            var start = setInterval(function() {
+                    if (count <= 0) {
+                        clearInterval(start)
+                    } else {
+                        Game.giveLetter()
+                        count--
+                    }
+                }, 100)
+                // $("#letterGet").click()
         })
 
-        this.addResponseCallback("score-update", function (data) {
+        this.addResponseCallback("score-update", function(data) {
             //console.log("tutaj będzie wyświetlanie zaktualizowanych wyników", data)
             Game.scoreboard.myScore = parseInt(data.mine)
             Game.scoreboard.opponentScore = parseInt(data.opponents)
 
             $("#scoreboard").html("<h3>" + Game.scoreboard.myName + " : " + Game.scoreboard.myScore + "</h3>" + "<h3>" + Game.scoreboard.opponentName + " : " + Game.scoreboard.opponentScore + "</h3>")
         })
-        this.addResponseCallback("board-update", function (data) {
+        this.addResponseCallback("board-update", function(data) {
             //console.log("tutaj będzie aktualizacja planszy", data)
             Game.updateBoard(data.board)
         })
-        this.addResponseCallback("nickname-update", function (data) {
+        this.addResponseCallback("nickname-update", function(data) {
             //console.log("otrzymano info o nazwach graczy", data)
             Game.scoreboard.myName = data.mine
             Game.scoreboard.opponentName = data.opponents
             $("#scoreboard").html("<h3>" + Game.scoreboard.myName + " : " + Game.scoreboard.myScore + "</h3>" + "<h3>" + Game.scoreboard.opponentName + " : " + Game.scoreboard.opponentScore + "</h3>")
+            if (data.opponents != "") {
+                if (Game.myTurn) {
+                    $("#exchangeMode").prop("disabled", false)
+                    $("#skip").prop("disabled", false)
+                    $("#turnStatus").text("TWOJA TURA")
+                } else {
+                    $("#exchangeMode").prop("disabled", true)
+                    $("#skip").prop("disabled", true)
+                    $("#turnStatus").text("TURA PRZECIWNIKA")
+                }
+            }
         })
-        this.addResponseCallback("turn-update", function (data) {
+        this.addResponseCallback("turn-update", function(data) {
             if (data.myTurn) {
                 Game.myTurn = true
                 $("#exchangeMode").prop("disabled", false)
                 $("#skip").prop("disabled", false)
+                $("#turnStatus").text("TWOJA TURA")
                 console.log("teraz moja tura")
             } else {
                 Game.myTurn = false
                 $("#exchangeMode").prop("disabled", true)
                 $("#skip").prop("disabled", true)
+                $("#turnStatus").text("TURA PRZECIWNIKA")
                 console.log("już nie moja tura")
             }
         })
 
-        this.addResponseCallback("send-nickname", function (data) {
+        this.addResponseCallback("send-nickname", function(data) {
             if (!data.accepted) {
                 let nick = prompt("Nickname taken, try again:")
                 SocketHander.emit("send-nickname", { nickname: nick })
@@ -75,16 +99,16 @@ var SocketHander = {
     },
 
     callbacks: [],
-    responseHandlers: function () {
+    responseHandlers: function() {
         for (let event of EventList) {
-            this.client.on(event + "-resp", function (data) {
+            this.client.on(event + "-resp", function(data) {
                 console.log("Event socketowy: " + event, data)
                 if (SocketHander.callbacks[event]) SocketHander.callbacks[event](data)
             })
         }
     },
 
-    emit: function (event, data, callback) {
+    emit: function(event, data, callback) {
         if (!EventList.includes(event)) { console.log(`Niepoprawny event socketowy: ${event}`); return }
 
         if (callback) this.callbacks[event] = callback
