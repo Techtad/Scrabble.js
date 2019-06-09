5f299c8a81e0a84a82e0039c416555c0913acb7c
Shwastoo <jakubszwast@gmail.com>
2019-06-09 16:04:13 +0200

    próba wgrania modelu choc srednio udana

---
+++ client/js/table.js
@@ -0,0 +1,23 @@
+class Table {
+    constructor() {
+
+        var mtlLoader = new THREE.MTLLoader()
+        mtlLoader.setTexturePath("model/")
+        mtlLoader.setPath("model/")
+        mtlLoader.load("wooden-coffe-table.mtl", function(materials) {
+            materials.preload()
+
+            var objLoader = new THREE.OBJLoader()
+            objLoader.setMaterials(materials)
+            objLoader.setPath("model/")
+            objLoader.load("wooden-coffe-table.obj", function(obj) {
+                obj.scale.set(110, 110, 110)
+                obj.position.set(70, -144, 70)
+                console.log(obj)
+                Game.scene.add(obj)
+            })
+
+        })
+    }
+
+}
