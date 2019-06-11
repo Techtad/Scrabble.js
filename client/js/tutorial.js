be88e851b5422b73fef366e3ca9a258b97c7cb23
Techtad <smigolpol@gmail.com>
2019-06-11 18:51:44 +0200

    Inne przyciski na końcu tutorialu.

--- client/js/tutorial.js
+++ client/js/tutorial.js
@@ -3,6 +3,7 @@ var TutorialStages = [
     { html: "Stage 2" },
     { html: "Stage 3<br><br><br>" },
     { html: "<br><br>Stage 4" },
+    { html: "And that's all.<br>You now know how to play our game. We hope you enjoy it.<br>Good luck!" }
 ]
 
 var Tutorial = {
@@ -20,7 +21,7 @@ var Tutorial = {
         if (this.dialog) this.dialog.remove()
         this.dialog = $("<div>")
 
-        this.dialog.html(stageData.html).dialog({
+        let dialogOptions = {
             modal: true,
             resizable: false,
             draggable: false,
@@ -29,16 +30,27 @@ var Tutorial = {
                 if (Tutorial.stage != TutorialStages.length - 1) Tutorial.nextStage()
                 $(this).remove()
             },
-            buttons: {
-                Next: function (event, ui) {
-                    $(this).dialog("close")
-                },
-                "Skip Tutorial": function () {
-                    PageUtils.setCookie("tutorialskipped", true)
-                    $(this).remove()
-                }
-            }
-        })
+        }
+
+        let lastStage = (Tutorial.stage == TutorialStages.length - 1)
+
+        let leftButton = lastStage ? "Close" : "Next"
+        let rightButton = lastStage ? "Don't Show Again" : "Skip Tutorial"
+        dialogOptions.buttons = {}
+        dialogOptions.buttons[leftButton] = function (event, ui) {
+            $(this).dialog("close")
+        }
+        dialogOptions.buttons[rightButton] = function () {
+            PageUtils.setCookie("tutorialskipped", true)
+            $(this).remove()
+        }
+
+        if (lastStage) {
+            dialogOptions.width = 360
+            dialogOptions.minWidth = 360
+        }
+
+        this.dialog.html(stageData.html).dialog(dialogOptions)
     },
 
     clear: function () {
