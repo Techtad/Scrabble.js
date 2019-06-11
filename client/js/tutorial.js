3a3663132b6511a5c82ccd8855fc4f6d04fce106
Techtad <smigolpol@gmail.com>
2019-06-11 21:04:16 +0200

    Tutorial skończony. (Wreszcie!)

--- client/js/tutorial.js
+++ client/js/tutorial.js
@@ -1,14 +1,27 @@
 var TutorialStages = [
     { html: "Welcome to Scrabble!<br>This short tutorial will teach you how to play." },
-    { html: "Stage 2", pos: [100, 300] },
-    { html: "Stage 3<br><br><br>", arrow: { pos: [50, 50], rot: -45 } },
-    { html: "<br><br>Stage 4" },
-    { html: "And that's all.<br>You now know how to play our game. We hope you enjoy it.<br>Good luck!" }
+    { html: "Scrabble is a competitive game where the players build words by arraning letter pieces on the tiles of the board." },
+    { html: "Each player draws 15 random letter pieces at the beginning of the game.<br>Your pieces are located on your <b>tray</b>.<br>You can select one at any time by clicking it with the <b>Left Mouse Button</b>.", arrow: { pos: ["calc(50vw - 60px)", "66vh"], rot: 90 }, width: 420, pos: { my: "center top", at: "center top+100" } },
+    { html: "Having a piece selected, you can swap it with another one on your tray by clicking on the other piece with the <b>Right Mouse Button</b>.<br>When it's your turn, you can place it on the board by clicking on a tile on the board with the <b>Left Mouse Button</b>.", width: 440 },
+    { html: "You can arrange words on the board from left to right or from top to bottom.<br>The first word in the game must be placed on the <b>yellow tile</b> in the middle of the board.<br>Each next word has to contain at least one letter already present on the board.", width: 440, pos: { my: "left center", at: "left center" }, arrow: { pos: ["calc(50vw - 60px)", "calc(50vh - 115px)"], rot: 90 }, },
+    { html: "After you've placed your pieces, click the <b>PLACE WORD</b> button to place it. If it's correct, the word will be placed on the board and you will have completed your turn.<br>You will receive points equivalent to the length of the word.", width: 520, arrow: { pos: ["110px", "85px"], rot: 180 } },
+    { html: "If you make a mistake, you can retrieve your pieces my clicking the <b>RESET WORD</b> button.", width: 420, arrow: { pos: ["215px", "85px"], rot: 180 } },
+    { html: "If you can't think of any word to make, you can choose to redraw some of your pieces using the <b>REDRAW LETTERS</b> button.", width: 440, arrow: { pos: ["110px", "145px"], rot: 180 } },
+    { html: "Once you're in <b>redraw mode</b>, select the pieces you want to exchange on your tray.<br>Then you redraw then using the <b>REDRAW!</b> button.<br>This uses up your turn.", width: 440, arrow: { pos: ["215px", "145px"], rot: 180 } },
+    { html: "The game ends when both players have <b>redrawn twice in a row</b>.<br>The player with the most points wins.", width: 430 },
+    { html: "That is all.<br>You now how to play our game.<br>Have fun!" }
 ]
 
 var Tutorial = {
     start: function () {
         if (!PageUtils.getCookie("tutorialskipped")) {
+            Game.camera.position.set(70, 170, 300)
+            Game.camera.rotation.set(-0.64, 0, 0)
+            Game.camera.updateProjectionMatrix()
+
+            $("#showUI").click()
+            $("#showScr").click()
+
             this.stage = -1
             this.nextStage()
         }
@@ -24,7 +37,7 @@ var Tutorial = {
         let dialogOptions = {
             modal: true,
             resizable: false,
-            draggable: false,
+            draggable: true,
             title: "Tutorial",
             close: function (event, ui) {
                 if (Tutorial.arrow) { Tutorial.arrow.remove(); Tutorial.arrow = null }
@@ -41,8 +54,8 @@ var Tutorial = {
             Tutorial.arrow = $("<img id='test'>")
             Tutorial.arrow.attr("src", "/gfx/tutorial-arrow.png")
             Tutorial.arrow.addClass("tutorialArrow")
-            Tutorial.arrow.css("left", `${stageData.arrow.pos[0]}px`)
-            Tutorial.arrow.css("top", `${stageData.arrow.pos[1]}px`)
+            Tutorial.arrow.css("left", stageData.arrow.pos[0])
+            Tutorial.arrow.css("top", stageData.arrow.pos[1])
             Tutorial.arrow.css("transform", `rotate(${stageData.arrow.rot}deg)`)
             $("#game").append(Tutorial.arrow)
         }
@@ -61,6 +74,8 @@ var Tutorial = {
             $(this).remove()
             Tutorial.dialog = null
         }
+
+        if (stageData.width) dialogOptions.width = stageData.width
 
         if (lastStage) {
             dialogOptions.width = 360
